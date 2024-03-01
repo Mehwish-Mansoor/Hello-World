997539c005ec97402fd2d3e0d67c8471da1cd1ee
Mehwish-Mansoor <160300292+Mehwish-Mansoor@users.noreply.github.com>
2024-03-01 10:16:20 +0500

    Add files via upload

---
+++ qno3.js
@@ -0,0 +1,6 @@
+"use strict";
+// 3.Name Case:Store a person's name in a variable, and then print that person' name in lowercase, uppercase, and titlecase.
+let firstName = "MehwishMansoor";
+console.log(firstName.toLowerCase());
+console.log(firstName.toUpperCase());
+console.log(firstName.charAt(0).toUpperCase() + firstName.slice(1));
