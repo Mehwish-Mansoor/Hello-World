2e03dce89777f507d2cd041de62420e925929312
Mehwish-Mansoor <160300292+Mehwish-Mansoor@users.noreply.github.com>
2024-03-01 10:09:34 +0500

    Add files via upload

--- Qno2.js
+++ Qno2.js
@@ -1,5 +1,5 @@
 "use strict";
-// Persona Message: Store a person's name in a variable, and print a person to that person . your message should be simple, such as," Hello Eric,
-// would you like to learn some python?""
-let firstName = "Eric";
-console.log(`"Hello ${firstName}would you like to learn some pyhton today"`);
+Object.defineProperty(exports, "__esModule", { value: true });
+// 2. Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+let personName = "Eric";
+console.log(`"Hello ${personName}, would you like to learn some python today?" `);
