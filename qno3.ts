2e5dd15b85a29ad685ec4b808eba0ea40d55daa9
Mehwish-Mansoor <160300292+Mehwish-Mansoor@users.noreply.github.com>
2024-03-01 12:27:14 +0500

    Delete qno3.ts

--- qno3.ts
+++
@@ -1,6 +0,0 @@
-// 3.Name Case:Store a person's name in a variable, and then print that person' name in lowercase, uppercase, and titlecase.
-
-let firstName:string="MehwishMansoor";
-console.log(firstName.toLowerCase())
-console.log(firstName.toUpperCase())
-console.log(firstName.charAt(0).toUpperCase() + firstName.slice(1));
